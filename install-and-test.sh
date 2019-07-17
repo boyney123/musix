6f887f6a27dc862e733469e9831800295bbed3db
David Boyne <davidboyne123@hotmail.co.uk>
2019-07-17 17:17:35 +0100

    Removing files for now

--- install-and-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-dir=$(pwd)
-cd "$dir/songs-api" && npm install && npm test .
-cd "$dir/songs-client" && npm install && CI=true npm test -- --coverage
-# cd "$dir/songs-subscriber" && npm install && npm test
